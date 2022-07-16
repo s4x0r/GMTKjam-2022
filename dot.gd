d3aa677ddcaf3c880ab5fde4089213ebb5ec0940
s4x0r <s4x0r.101@gmail.com>
2022-07-15 19:06:38 -0500

    finished dice dot moving
    unfold.tcsn

--- dot.gd
+++ dot.gd
@@ -5,17 +5,24 @@ extends Sprite
 # var a = 2
 # var b = "text"
 
-
+var moving
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	moving=false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	if !Input.is_action_pressed("click"):
+		moving=false
+	if moving:
+		self.position-=(self.get_global_position()-self.get_global_mouse_position())
 #	pass
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	print(event)
+	#print(event)
 	if event is InputEventMouseMotion:
-		print("True")
+		if Input.is_action_pressed("click"):
+			moving=true
+
