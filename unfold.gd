c850314a08036ab06046724cb623ac674d22eea7
s4x0r <s4x0r.101@gmail.com>
2022-07-15 19:51:20 -0500

    more work on unfold.tcsn

--- unfold.gd
+++ unfold.gd
@@ -6,11 +6,27 @@ extends Node2D
 # var b = "text"
 
 var sides =[0,0,0,0,0,0]
+var face=1
+
+var rng = RandomNumberGenerator.new()
+
 
 func fold():
 	for i in range(1,6):
 		sides[i-1]=get_node("facemap/side "+str(i)).get_overlapping_areas().size()
 	print(sides)
+
+func next():
+	face +=1
+	if face ==7:
+		face = 1
+	$ViewportContainer/Viewport/camera.position-=($ViewportContainer/Viewport/camera.get_global_position()-get_node("facemap/side "+face).get_global_position())
+
+func roll():
+	return sides[rng.randi_range(0, sides.size()-1)]
+
+func rollem():
+	print(roll())
 
 
 # Called when the node enters the scene tree for the first time.
